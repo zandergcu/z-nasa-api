5b5416297b3ad7947d02acae50ab5b901bdaf2a6
Zander <amacke201@caledonian.ac.uk>
2019-02-06 13:04:01 +0000

    updated meta

--- routes/index.js
+++ routes/index.js
@@ -4,27 +4,38 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'VueJS Practice'
+    title: 'VueJS Practice',
+    description: 'NASA API practice.',
+    url: 'https://z-nasa-api.herokuapp.com/'
   });
 });
 
 /* GET apod page. */
 router.get('/apod', function(req, res, next) {
   res.render('apod', {
-    title: 'Astronomy Picture Of the Day'
+    title: 'Astronomy Picture Of the Day',
+    description: 'NASA API practice. Get APOD.',
+    url: 'https://z-nasa-api.herokuapp.com/apod'
   });
 });
 
 /* GET asteroids page. */
 router.get('/asteroids', function(req, res, next) {
   res.render('asteroids', {
-    title: 'Nasa API Asteroids'
+    title: 'Nasa API Asteroids',
+    description: 'NASA API practice. Get asteroids.',
+    url: 'https://z-nasa-api.herokuapp.com/asteroids'
   });
 });
 
 /* POST to apod to get results */
 router.post('/apod', function(req, res, next) {
-  res.render('results', { title: 'Results', form: req.body });
+  res.render('results', {
+    title: 'Results',
+    description: 'NASA API practice. APOD results',
+    url: 'https://z-nasa-api.herokuapp.com/apod',
+    form: req.body
+  });
 });
 
 module.exports = router;
